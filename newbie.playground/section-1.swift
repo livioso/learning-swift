190628d186ab7e981d363e8a6ed37fe30a728059
Livio Bieri <livio@livio.li>
2014-06-08 23:02:38 +0200

    Some basic loops

--- newbie.playground/section-1.swift
+++ newbie.playground/section-1.swift
@@ -3,3 +3,27 @@
 import Cocoa
 
 var str = "Hello, playground. Please don't crash."
+
+var strLooped = ""
+for each:Character in str {
+    strLooped += each
+}
+
+var sumZeroToTen = 0;
+for i in 0..10 {
+    sumZeroToTen += i
+}
+
+sumZeroToTen = 0;
+
+// equals to 0..10
+for var i = 0; i < 10; i++ {
+    sumZeroToTen += i
+}
+
+let max = 10
+var counter = 0
+while(max != counter) {
+    counter++
+}
+
