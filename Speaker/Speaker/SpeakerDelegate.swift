c62b185e693d0d533e30c6db6d9124d5d31c2ef3
Livio Bieri <livio@livio.li>
2014-06-23 11:13:15 +0200

    Speaker example app continued.

---
+++ Speaker/Speaker/SpeakerDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  SpeakerDelegate.swift
+//  Speaker
+//
+//  Created by Livio Bieri on 22/06/14.
+//  Copyright (c) 2014 Livio Bieri. All rights reserved.
+//
+
+import Foundation
+import Cocoa
+
+class SpeakerDelegate: NSObject {
+    
+    
+    @IBAction func start(sender: NSButton) {
+        var msgBox: NSAlert = NSAlert()
+        println("Hallo")
+    }
+    
+    @IBAction func stop(sender: NSButton) {
+        
+    }
+}
---
+++ Speaker/Speaker/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  Speaker
+//
+//  Created by Livio Bieri on 22/06/14.
+//  Copyright (c) 2014 Livio Bieri. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
