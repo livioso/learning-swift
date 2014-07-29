292c245a5893a3ebad8a9ba704444ef48d79cd73
Livio Bieri <livio@livio.li>
2014-07-29 18:26:42 +0200

    Given the array controller a managed object context

---
+++ GarageOfCars/GarageOfCars/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  GarageOfCars
+//
+//  Created by Livio Bieri on 29/07/14.
+//  Copyright (c) 2014 Livio Bieri. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
