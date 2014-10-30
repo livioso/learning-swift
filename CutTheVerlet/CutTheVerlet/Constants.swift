886261528530ea28408b68b0f1fd0f28e235c997
Livio Bieri <livio@livio.li>
2014-10-30 10:41:54 +0100

    Necessary constants added

--- CutTheVerlet/CutTheVerlet/Constants.swift
+++ CutTheVerlet/CutTheVerlet/Constants.swift
@@ -8,7 +8,23 @@
 
 import UIKit
 
-//MARK: constants
+let BackgroundImage = "Background"
+let GroundImage = "Ground"
+let WaterImage = "Water"
+let RopeTextureImage = "RopeTexture"
+let RopeHolderImage = "RopeHolder"
+let CrocMouthClosedImage = "CrocMouthClosed"
+let CrocMouthOpenImage = "CrocMouthOpen"
+let CrocMaskImage = "CrocMask"
+let PrizeImage = "Pineapple"
+let PrizeMaskImage = "PineappleMask"
+
+let BackgroundMusicSound = "CheeZeeJungle.caf"
+let SliceSound = "Slice.caf"
+let SplashSound = "Splash.caf"
+let NomNomSound = "NomNom.caf"
+
+let RopeDataFile = "RopeData.plist"
 
 
 
