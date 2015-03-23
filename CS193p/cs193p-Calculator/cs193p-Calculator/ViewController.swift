b7d25f2a81af1b71569851c641f93084c9914aeb
Livio Bieri <livio@livio.li>
2015-03-23 21:46:17 +0100

    CS193p-Calculator initial commit

---
+++ CS193p/cs193p-Calculator/cs193p-Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  cs193p-Calculator
+//
+//  Created by Livio Bieri on 21/03/15.
+//  Copyright (c) 2015 Livio Bieri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
