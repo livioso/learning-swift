cf838a848be9dfa4f47a5b57b26fdbb88201cfe6
Livio Bieri <livio@livio.li>
2015-08-14 18:10:25 +0200

    Step one of the shity calculator...

--- CS193p/cs193p-Calculator/cs193p-Calculator/ViewController.swift
+++ CS193p/cs193p-Calculator/cs193p-Calculator/ViewController.swift
@@ -10,16 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+  @IBOutlet weak var display: UILabel!
+  
+  var userIsTheMiddleOfTyping: Bool = false
+
+  @IBAction func appendDigit(sender: UIButton) {
+    if userIsTheMiddleOfTyping {
+      let digit = sender.currentTitle!
+    } else {
+      display.text = display.text! + digit
+      userIsTheMiddleOfTyping = true
+    }
   }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-
-
 }
-
