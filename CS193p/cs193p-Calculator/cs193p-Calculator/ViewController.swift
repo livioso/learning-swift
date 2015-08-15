e1e93829e4a9db0602993af8524d6b5381e13df4
Livio Bieri <livio@livio.li>
2015-08-15 16:32:22 +0200

    Step two of the shity calculator

--- CS193p/cs193p-Calculator/cs193p-Calculator/ViewController.swift
+++ CS193p/cs193p-Calculator/cs193p-Calculator/ViewController.swift
@@ -12,13 +12,61 @@ class ViewController: UIViewController {
 
   @IBOutlet weak var display: UILabel!
   
-  var userIsTheMiddleOfTyping: Bool = false
-
-  @IBAction func appendDigit(sender: UIButton) {
+  var userIsTheMiddleOfTyping = false
+  
+  @IBAction func operate(sender: UIButton, forEvent event: UIEvent) {
+    let operation = sender.currentTitle!
     if userIsTheMiddleOfTyping {
-      let digit = sender.currentTitle!
-    } else {
+      enter()
+    }
+    
+    switch operation {
+    case "×": performOperation({$0 * $1})
+    case "÷": performOperation({$1 / $0})
+    case "+": performOperation({$0 + $1})
+    case "−": performOperation({$1 - $0})
+    case "√": performOperationSing({sqrt($0)})
+    default: break
+    }
+  }
+  
+  func performOperation(operation: (Double,Double) -> Double) {
+    if operandStack.count >= 2 {
+      displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
+      enter()
+    }
+  }
+  
+  func performOperationSing(operation: (Double) -> Double) {
+    if operandStack.count >= 1 {
+      displayValue = operation(operandStack.removeLast())
+      enter()
+    }
+  }
+
+  var operandStack: Array<Double> = Array<Double>()
+  @IBAction func enter() {
+    userIsTheMiddleOfTyping = false
+    operandStack.append(displayValue)
+    print("\(operandStack)")
+  }
+  
+  var displayValue: Double {
+    get {
+      return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+    }
+    set {
+      display.text = "\(newValue)"
+      userIsTheMiddleOfTyping = false
+    }
+  }
+  
+  @IBAction func appendDigit(sender: UIButton) {
+    let digit = sender.currentTitle!
+    if userIsTheMiddleOfTyping {
       display.text = display.text! + digit
+    } else {
+      display.text = digit
       userIsTheMiddleOfTyping = true
     }
   }
