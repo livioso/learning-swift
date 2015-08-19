767d76acd6ba175252a7b94bcb92e07a10df19f0
Livio Bieri <livio@livio.li>
2015-08-19 15:48:18 +0200

    Happiness Smile :) initially implemented

---
+++ CS193p/cs193p-Happiness/Happiness/Happiness/FaceView.swift
@@ -0,0 +1,85 @@
+//
+//  FaceView.swift
+//  Happiness
+//
+//  Created by Livio Bieri on 19/08/15.
+//  Copyright © 2015 Livio Bieri. All rights reserved.
+//
+
+import UIKit
+
+class FaceView: UIView {
+
+    var lineWidth: CGFloat = 3 {didSet {setNeedsDisplay() }}
+    var color: UIColor = UIColor.blueColor() {didSet {setNeedsDisplay() }}
+
+    var faceCenter: CGPoint {
+        return convertPoint(center, fromView: superview)
+    }
+
+    var faceRadius: CGFloat {
+        return min(bounds.size.width, bounds.size.height) / 2 * 0.9
+    }
+
+    private struct Scaling {
+        static let FaceRadiusToEyeRadiusRatio: CGFloat = 10
+        static let FaceRadiusToEyeOffsetRatio: CGFloat = 3
+        static let FaceRadiusToEyesSeperationRatio: CGFloat = 1.5
+        static let FaceRadiusToMouthWidthRatio: CGFloat = 1
+        static let FaceRadiusToMouthHeightRatio: CGFloat = 3
+        static let FaceRadiusToMouthOffsetRatio: CGFloat = 3
+    }
+
+    private enum Eye {case Left, Right}
+
+    private func bezierPathForEye(whichEye: Eye) -> UIBezierPath {
+        let eyeRadius = faceRadius / Scaling.FaceRadiusToEyeRadiusRatio
+        let eyeVerticalOffset = faceRadius / Scaling.FaceRadiusToEyeOffsetRatio
+        let eyeHorizontalSeparation = faceRadius / Scaling.FaceRadiusToEyesSeperationRatio
+
+        var eyeCenter = faceCenter
+        eyeCenter.y -= eyeVerticalOffset
+
+        switch(whichEye) {
+        case .Left: eyeCenter.x -= eyeHorizontalSeparation / 2
+        case .Right: eyeCenter.x += eyeHorizontalSeparation / 2
+        }
+
+        let path = UIBezierPath(arcCenter: eyeCenter, radius:eyeRadius, startAngle: 0, endAngle: CGFloat(2*M_PI), clockwise: true)
+        path.lineWidth = lineWidth
+        return path
+    }
+
+    private func bezierPathForSmile(fractionOfMaxSmile: Double) -> UIBezierPath {
+        let mouthWidth = faceRadius / Scaling.FaceRadiusToMouthWidthRatio
+        let mouthHeight = faceRadius / Scaling.FaceRadiusToMouthHeightRatio
+        let mouthVeriticalOffset = faceRadius / Scaling.FaceRadiusToMouthOffsetRatio
+
+        let smileHeight = CGFloat(max(min(fractionOfMaxSmile, 1), -1)) * mouthHeight
+
+        let start = CGPoint(x: faceCenter.x - mouthWidth / 2, y: faceCenter.y + mouthVeriticalOffset)
+        let end = CGPoint(x: start.x + mouthWidth, y: start.y)
+        let cp1 = CGPoint(x: start.x + mouthWidth / 3, y: start.y + smileHeight)
+        let cp2 = CGPoint(x: end.x - mouthWidth / 3, y: cp1.y)
+
+        let path = UIBezierPath()
+        path.moveToPoint(start)
+        path.addCurveToPoint(end, controlPoint1: cp1, controlPoint2: cp2)
+        path.lineWidth = lineWidth
+
+        return path
+    }
+
+    override func drawRect(rect: CGRect)    {
+        let facePath = UIBezierPath(arcCenter: faceCenter, radius: faceRadius, startAngle: 0, endAngle: CGFloat(2*M_PI), clockwise: true);
+        facePath.lineWidth = lineWidth
+        color.set()
+        facePath.stroke()
+
+        bezierPathForEye(.Left).stroke()
+        bezierPathForEye(.Right).stroke()
+        let smilines   = 0.4
+        let smilePath = bezierPathForSmile(smilines)
+        smilePath.stroke()
+    }
+}
---
+++ CS193p/cs193p-Happiness/Happiness/Happiness/HappinessViewController.swift
@@ -0,0 +1,13 @@
+//
+//  HappinessViewController.swift
+//  Happiness
+//
+//  Created by Livio Bieri on 19/08/15.
+//  Copyright © 2015 Livio Bieri. All rights reserved.
+//
+
+import UIKit
+
+class HappinessViewController: UIViewController {
+
+}
