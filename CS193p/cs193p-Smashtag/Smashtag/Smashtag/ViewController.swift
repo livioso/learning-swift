326b3d9a93ec4bff49d57c68ea35303773a9d1a6
Livio Bieri <livio@livio.li>
2015-09-03 18:33:11 +0200

    Smashtag initial setup // lesson 10

---
+++ CS193p/cs193p-Smashtag/Smashtag/Smashtag/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Smashtag
+//
+//  Created by Livio Bieri on 03/09/15.
+//  Copyright © 2015 Livio Bieri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
